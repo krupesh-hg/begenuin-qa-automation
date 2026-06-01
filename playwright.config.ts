70ba11661b409ec7fa3c76159ec1ca712a66b58e
Krupesh Gandhi <133755387+krupesh-hg@users.noreply.github.com>
2026-06-01 21:09:44 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,36 +1,43 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 dotenv.config();
+
+// Check if session file exists
+const sessionExists = fs.existsSync('./session.json');
 
 export default defineConfig({
   testDir: './',
   testMatch: '*.spec.ts',
   timeout: 40_000,
   retries: 1,
+
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['list'],
   ],
+
   use: {
+    channel: 'msedge',
     headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    // If session.json exists, all tests start already logged in!
+    storageState: sessionExists ? 'session.json' : undefined,
   },
+
   projects: [
     {
       name: 'begenuin',
       use: {
-        ...devices['Desktop Chrome'],
         baseURL: 'https://begenuin.com',
       },
     },
     {
       name: 'carlist',
       use: {
-        ...devices['Desktop Chrome'],
-        baseURL: 'https://carlist.begenuin.com',
+        baseURL: 'https://community.carlist.my',
       },
     },
   ],
+
   outputDir: 'test-results/',
 });
